0a4f8f56ee28dc8824ef78b715ea73b0faacc629
Alexandre Borela <alexandre@borela.tech>
2016-12-29 05:12:12 -0200

    Simplify web settings

--- web/src/index.js
+++ web/src/index.js
@@ -15,94 +15,35 @@ let HtmlPlugin = require('html-webpack-plugin')
 let path = require('path')
 let webpack = require('webpack')
 
-/**
- * Actual project being built with webpack.
- */
+let createBasicSettings = require('./shared')
+
+// The parent script must be the actual webpack config file at the root of the
+// project so that we can determine the project’s directory.
 const PROJECT_DIR = path.dirname(module.parent.filename)
 
-/**
- * Directory where the bundles will reside.
- */
-const BUILD_DIR = path.join(PROJECT_DIR, 'build')
+// Settings skeleton.
+let settings = createBasicSettings(PROJECT_DIR)
 
-/**
- * Paths used to search for modules and project’s files.
- */
-const MODULE_PATHS = [
-  path.join(__dirname, '..', 'node_modules'),
-  path.join(PROJECT_DIR, 'node_modules'),
-  path.join(PROJECT_DIR, 'src'),
-  './node_modules',
-  './src'
-]
+settings.entry.push('babel-polyfill')
+settings.entry.push('main')
 
-/**
- * Various plugins used during development and production.
- */
-let plugins = []
+// The hash in the end is used by HtmlPlugin.
+settings.output.filename = 'script.js?[hash:8]'
 
-plugins.push(
+// Main entry point to the single page app.
+settings.plugins.push(
   new HtmlPlugin({
     template: path.join(PROJECT_DIR, 'src', 'index.html')
   })
 )
 
 if (!IS_DEV) {
-  plugins.push(
+  settings.plugins.push(
     new webpack.optimize.UglifyJsPlugin({
       comments: false
     })
   )
 }
 
-/**
- * Actual webpack settings.
- */
-module.exports = {
-  resolve: {
-    modules: MODULE_PATHS,
-    extensions: [
-      '.css',
-      '.mcss',
-      '.msass',
-      '.mscss',
-      '.js',
-      '.jsx',
-      '.sass',
-      '.scss'
-    ],
-  },
-  resolveLoader: {
-    modules: MODULE_PATHS,
-    moduleExtensions: ['-loader']
-  },
-  entry: [
-    'babel-polyfill',
-    'main'
-  ],
-  output: {
-    path: BUILD_DIR,
-    filename: 'script.js?[hash:8]'
-  },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: BUILD_DIR,
-    historyApiFallback: true,
-    port: 3000
-  },
-  performance: {
-    hints: IS_DEV ? false : 'warning'
-  },
-  module: {
-    rules: [
-      require('./asset'),
-      require('./css-module'),
-      require('./css'),
-      require('./html'),
-      require('./js'),
-      require('./sass-module')(MODULE_PATHS),
-      require('./sass')(MODULE_PATHS)
-    ]
-  },
-  plugins
-}
+// The final settings tweaked for web apps.
+module.exports = settings
